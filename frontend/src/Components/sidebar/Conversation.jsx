38d0bf85ad22992b56c242073b3c5c563d742877
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-02-17 13:57:07 +0530

    Sidebar done

--- frontend/src/Components/sidebar/Conversation.jsx
+++ frontend/src/Components/sidebar/Conversation.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import useConversation from "../../zustand/useConversation";
 
-function Conversation() {
+function Conversation({conversation,lastIdx,emoji}) {
+    const { selectedConversation, setSelectedConversation } = useConversation();//hook
+
+	const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
         <div
-            className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-        `}
-        >
-            <div className={`avatar `}>
+				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+				onClick={() => setSelectedConversation(conversation)}
+		>
+            <div className={`avatar online`}>
                 <div className='w-12 rounded-full'>
-                    <img src="https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg" alt='user avatar' />
+                    <img src={conversation.profilePic} alt='user avatar' />
                 </div>
             </div>
 
             <div className='flex flex-col flex-1'>
                 <div className='flex gap-3 justify-between'>
-                    <p className='font-bold text-gray-200'>Himanshu</p>
-                    <span className='text-xl'>😂</span>
+                    <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+                    <span className='text-xl'>{emoji}</span>
                 </div>
             </div>
         </div>
 
-        <div className='divider my-0 py-0 h-1' />
+        {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     </>
   )
 }
---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetConversations = () => {
+	const [loading, setLoading] = useState(false);
+	const [conversations, setConversations] = useState([]);
+    console.log("Conversations...",conversations);
+
+	useEffect(() => {
+		const getConversations = async () => {
+			setLoading(true);
+			try {
+				const res = await fetch("/api/users");
+				const data = await res.json();
+				if (data.error) {
+					throw new Error(data.error);
+				}
+				setConversations(data);
+			} catch (error) {
+				toast.error(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		getConversations();
+	}, []);// run once thats why empty dependency array
+
+	return { loading, conversations };
+};
+export default useGetConversations;
