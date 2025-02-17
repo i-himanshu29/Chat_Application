169511cba505e2d457d961d70f16193bf5977da1
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-02-17 20:25:59 +0530

    All done with socket.io and setting deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express"
 // const dotenv = require("dotenv")
 import dotenv from "dotenv"
+import path from "path";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -10,15 +11,20 @@ import cookieParser from "cookie-parser";
 import {app,server} from "./socket/socket.js"
 // const app = express();
 
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();// for deploy
 
-dotenv.config();
+
 
 app.use(express.json());// to parse the incoming requests with json payloads (from) req.body
 
 //cookiwe parser
 app.use(cookieParser());
+
+
 
 // app.get("/",(req,res)=>{
 //     // root route http://localhost:5000/
@@ -39,6 +45,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist"))) // for deploy
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // app.listen(PORT,()=>{
 //     connectToMongoDB();
